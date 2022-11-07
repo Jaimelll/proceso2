ea539aa36b09812e35d41acf3263c95d717dd5da
Jaimelll <jaduran0102@gmail.com>
2022-11-07 14:53:04 -0500

    a postgres3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'admin/dashboard#index'
+  # comentario postesgres 3
   # Defines the root path route ("/")
   # root "articles#index"
 end
